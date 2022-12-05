0d8c0ad896f057ccbf481670d984193b8e2374ed
Marcel Noe <marcel.noe@1und1.de>
2022-12-05 09:05:14 +0100

    something still does not work

--- 05/crates1.py
+++ 05/crates1.py
@@ -1,20 +1,30 @@
+topCrates = []
 
 class Crate(object):
     
-    def __init__(self, value, previousCrate, nextCrate):
+    def __init__(self, pos, value, previousCrate, nextCrate):
         self.value = value
         self.nextCrate = None
         self.previousCrate = None
+        self.pos = pos
 
     def append(self, value):
         if self.nextCrate != None:
             self.nextCrate.append(value)
-            print("crate: traversing")
+            #print("crate: traversing")
         else:
-            self.nextCrate = Crate(value, self, None)
-            print("crate: appending")
+            self.nextCrate = Crate(self.pos, value, self, None)
+            #print("crate: appending")
 
-topCrates = []
+    def move(self, target):
+        # Link to top of target
+        if self.nextCrate != None:
+            self.nextCrate.previousCrate = None
+        topCrates[self.pos] = self.nextCrate
+        self.nextCrate = topCrates[target]
+        topCrates[target] = self
+        if self.nextCrate != None:
+            self.nextCrate.previousCrate = self
 
 with open("input.txt", "r") as infile:
     lines = infile.readlines()
@@ -28,7 +38,7 @@ with open("input.txt", "r") as infile:
         else:
             line += "*" 
 
-        topCrates.append(Crate(value, None, None))
+        topCrates.append(Crate(i, value, None, None))
     print(line) 
 
     # Now parse the following crates
@@ -43,3 +53,24 @@ with open("input.txt", "r") as infile:
             topCrates[i].append(value)
 
         print(line)
+
+    # Now parse instructions
+    for line in lines[10:]:
+        l = line.strip().split(" ")
+        _, count, _, source, _, target = l
+        count = int(count)
+        source = int(source)
+        target = int(target)
+        print(line)
+        for i in range(0, count):
+            print(i)
+            if topCrates[source-1] != None:
+                topCrates[source-1].move(target-1)
+
+line = ""
+for c in topCrates:
+    if c and c.value != None:
+        line += c.value
+    else:
+        print("*")
+print(line)
