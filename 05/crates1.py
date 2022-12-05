27648c68699ecc6cd149017485564f62b6890593
Marcel Noe <marcel.noe@1und1.de>
2022-12-05 11:20:45 +0100

    seems like found bug

--- 05/crates1.py
+++ 05/crates1.py
@@ -1,8 +1,14 @@
+import sys
+print("recursion limit: %i" % sys.getrecursionlimit())
+sys.setrecursionlimit(5000)
+
 topCrates = []
 
 class Crate(object):
     
     def __init__(self, pos, value, previousCrate, nextCrate):
+        if not value.isalpha():
+            value = None
         self.value = value
         self.nextCrate = None
         self.previousCrate = None
@@ -17,14 +23,56 @@ class Crate(object):
             #print("crate: appending")
 
     def move(self, target):
-        # Link to top of target
+        print("Moving %i to %i" % (self.pos, target))
+        oldpos = self.pos
+
+        # Skip empty grates
+        if self.value == None:
+            print("Empty container found")
+            if self.nextCrate != None:
+                print("removing empty crate")
+                self.nextCrate.move(target)
+            else:
+                raise Exception("Error")
+        else:
+            # Unlink from next container 
+            if self.nextCrate != None:
+                self.nextCrate.previousCrate = None
+
+            # Link next container to top of list
+            topCrates[self.pos] = self.nextCrate
+
+            # Now link the target top crate as next container
+            self.nextCrate = topCrates[target]
+
+            # Noe let us be the top container of the target
+            topCrates[target] = self
+
+            # Set pos to new pos
+            self.pos = target 
+
+            # And create the back link
+            if self.nextCrate != None:
+                self.nextCrate.previousCrate = self
+
+        # fix top list
+        if not topCrates[oldpos]:
+            topCrates[oldpos] = Crate(oldpos, "", None, None)
+
+    def getValues(self):
+        values = ""
+        if self.value:
+            values += self.value
         if self.nextCrate != None:
-            self.nextCrate.previousCrate = None
-        topCrates[self.pos] = self.nextCrate
-        self.nextCrate = topCrates[target]
-        topCrates[target] = self
-        if self.nextCrate != None:
-            self.nextCrate.previousCrate = self
+            #print(self.nextCrate)
+            values += self.nextCrate.getValues()
+        return values
+
+def printCrates():
+    i = 1
+    for topCrate in topCrates:
+        print("%i: %s" % (i, topCrate.getValues()))
+        i += 1
 
 with open("input.txt", "r") as infile:
     lines = infile.readlines()
@@ -63,10 +111,14 @@ with open("input.txt", "r") as infile:
         target = int(target)
         print(line)
         for i in range(0, count):
-            print(i)
+            print("%i %s -> %s" % (i, source, target))
+            print(topCrates[source-1].value)
             if topCrates[source-1] != None:
                 topCrates[source-1].move(target-1)
+            else:
+                print("Warning: Trying to move empty")
 
+        printCrates()
 line = ""
 for c in topCrates:
     if c and c.value != None:
