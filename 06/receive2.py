4a59217561c47875d1b40c74af393fd9093667c2
Marcel Noe <marcel.noe@1und1.de>
2022-12-06 08:12:08 +0100

    both versions working

--- 06/receive2.py
+++ 06/receive2.py
@@ -6,7 +6,7 @@ with open('input.txt', 'r') as infile:
 
 for i in range(0, len(line) + 1):
     print(i)
-    print(line[i:i+4])
-    if len(list(dict.fromkeys(list(line[i:i+4])))) == 4:
+    print(line[i:i+14])
+    if len(list(dict.fromkeys(list(line[i:i+14])))) == 14:
         print(i+1)
         break
