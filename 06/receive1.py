411b11ff823af6867df147a21ec0d528e9254583
Marcel Noe <marcel.noe@1und1.de>
2022-12-06 07:58:18 +0100

    first try

---
+++ 06/receive1.py
@@ -0,0 +1,11 @@
+#!/usr/local/bin/python
+
+with open('input.txt', 'r') as infile:
+    line = infile.readline()
+
+
+for i in range(0, len(line) + 1):
+    print(line[i:i+4])
+    if len(list(dict.fromkeys(list(line[i:i+4])))) == 4:
+        print(i+1)
+        break
