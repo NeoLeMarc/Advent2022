a95a92304a5f740553c15f93a6c68d339032b68a
Marcel Noe <marcel.noe@1und1.de>
2022-12-14 11:54:45 +0100

    plotting works, parsing not

--- 14/sand.py
+++ 14/sand.py
@@ -13,10 +13,9 @@ max_y = 0
 
 paths = []
 for line in lines:
-    path = line.split(" -> ")
-    paths.append(path)
-
-    for p in path:
+    spath = line.split(" -> ")
+    path = []
+    for p in spath:
         x, y = p.split(',')
         x = int(x)
         y = int(y)
@@ -31,27 +30,37 @@ for line in lines:
             min_y = y
 
         if y > max_y:
-            max_y = x
+            max_y = y 
+        pathElement = (x, y)
+        path.append(pathElement)
+    paths.append(path)
 
 print("%s %s / %s %s" % (min_x, max_x, min_y, max_y))
 
 cave = []
 
 # Iinitialize cave
-def printCave(cave):
+def initializeCave(cave):
     for x in range(0, max_x + 1):
+        cave.append([])
         for y in range(0, max_y + 1):
-            cave[x][y] = '.'
+            cave[x].append('.')
+
+initializeCave(cave)
 
 for path in paths:
     prevpoint = None
     for point in path:
+        print(point)
+        ## needs improvement
         if prevpoint:
             for x in range(prevpoint[0], point[0] + 1):
                 for y in range(prevpoint[1], point[1] + 1):
                     cave[x][y] = '#'
-        prevpoint = path
+        prevpoint = point
 
+print("%i - %i" % (min_x, max_x))
+print("%i - %i" % (min_y, max_y))
 def printCave(cave):
     for x in range(min_x, max_x + 1):
         line = ""
