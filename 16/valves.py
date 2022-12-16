0d6649c0e7cab9c7e259321a1bdf42680940cc47
Marcel Noe <marcel@marcel-noe.de>
2022-12-16 08:17:19 +0100

    almost

--- 16/valves.py
+++ 16/valves.py
@@ -22,7 +22,7 @@ for line in lines:
 #print(valves)
 curpos = 'AA'
 
-def calculateValue(seen, adjacent, timeLeft):
+def calculateValue(seen, adjacent, timeLeft, openValves):
     print(adjacent)
     if timeLeft == 0:
         print("No time left")
@@ -38,23 +38,49 @@ def calculateValue(seen, adjacent, timeLeft):
             print("Circle")
         else:
             seen.append(adj)
-            value = valves[adj][0] * timeLeft
-            ret.append((adj, value, timeLeft))
-            ret.extend(calculateValue(seen, valves[adj][1], timeLeft - 1))
+            if adj not in openValves: 
+                value = valves[adj][0] * timeLeft
+                ret.append((adj, value, timeLeft))
+                ret.extend(calculateValue(seen, valves[adj][1], timeLeft - 1, openValves))
     return ret
+
+totalValue = 0
+currentIncrement = 0
+openValves = []
 
 while timeLeft > 0:
     mostValuableValue = 0
     mostValuableKey = curpos 
-    ret = calculateValue([curpos], valves[curpos][1], timeLeft)
+    mostValuableTimeLeft = 0
+    ret = calculateValue([curpos], valves[curpos][1], timeLeft - 1, openValves)
+    print("--------------------------")
     print(ret)
+    print("Currently open valves: %s" % str(openValves))
+    print("Current increment: %i" % currentIncrement)
+    print("Time left: %i" % timeLeft)
 
     # get most valuable block
     for r in ret:
         if r[1] > mostValuableValue:
             mostValuableValue = r[1]
             mostValuableKey = r[0]
+            mostValuableTimeLeft = r[2]
 
     print(mostValuableKey)
     print(mostValuableValue)
-    break
+    print(timeLeft)
+
+    # Go down the most valuable path
+    currentIncrement += valves[mostValuableKey][0] 
+    curpos = mostValuableKey
+    openValves.append(curpos)
+
+    # Increment for the time it takes to move to new position:
+    for i in range(timeLeft - 1, mostValuableTimeLeft, -1):
+        totalValue += currentIncrement
+
+
+    timeLeft = mostValuableTimeLeft
+    totalValue += currentIncrement
+print("No time left")
+print(totalValue)
