2c2727de838cf1e91c3ce54f56814e9b8302374e
Marcel Noe <marcel@marcel-noe.de>
2022-12-17 19:24:30 +0100

    works in theory but not in practice and I guess, I'm done here

--- 17/tetris.py
+++ 17/tetris.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import pygame, random
+import pygame, random, sys
 
 
 colors =  [
@@ -12,11 +12,11 @@ colors =  [
     (180, 34, 122),
 ]
 
-height = 20
+height = 2000 
 width = 7
 injectionPoint = 1
 above = 3
-
+numberOfFigures = 0
 # Basic idea: 
 # https://levelup.gitconnected.com/writing-tetris-in-python-2a16bddb5318
 
@@ -32,10 +32,13 @@ class Figure:
                [[12, 13, 14, 15],  -3],
                [[5, 8, 9, 10, 13], -1],
                [[5, 9, 13, 12],  -1],
+               [[0, 4, 8, 12], 0],
                [[8, 9, 12, 13], -2],
              ]
 
     def __init__(self, x, y):
+        global numberOfFigures
+        global lastYPos
         self.x = x
         self.y = y
         self.type = Figure.nextFigure
@@ -44,6 +47,11 @@ class Figure:
         Figure.nextFigure += 1 # rotate through figures
         Figure.nextFigure = Figure.nextFigure % len(Figure.figures)
         self.offset = self.figures[self.type][1]
+        numberOfFigures += 1
+        if numberOfFigures == 2022:
+            print("Done!")
+            print(lastYPos)
+            sys.exit(1)
 
     def image(self):
         return self.figures[self.type][0]
@@ -57,7 +65,7 @@ class Tetris:
     width = 0
     x = 100
     y = 60
-    zoom = 20
+    zoom = 6 
     figure = None
 
     def __init__(self, height, width):
@@ -75,6 +83,8 @@ class Tetris:
 
     def new_figure(self):
         global lastYPos
+        global numberOfFigures
+        self.score = ("%i - %i" % (numberOfFigures, lastYPos))
         ## need to edit here
         self.figure = Figure(2, lastYPos - 2) # at least need to edit type
 
@@ -105,7 +115,7 @@ class Tetris:
                     for j in range(self.width):
                         self.field[i1][j] = self.field[i1 - 1][j]
 
-        self.score += lines ** 2
+        #self.score += lines ** 2
 
     def go_down(self):
         self.figure.y += 1
@@ -124,8 +134,8 @@ class Tetris:
         lastYPos = self.figure.y - self.figure.offset - 5 # figure offset + move to lower left coord
         #self.break_lines()
         self.new_figure()
-        if self.intersects():
-            self.state = "gameover"
+        #if self.intersects():
+        #    self.state = "gameover"
 
     def go_side(self, dx):
         old_x = self.figure.x
@@ -142,14 +152,14 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GRAY = (128, 128, 128)
 
-size = (400, 500)
+size = (400, 1000)
 screen = pygame.display.set_mode(size)
 
 pygame.display.set_caption("Elftris")
 
 done = False
 clock = pygame.time.Clock()
-fps = 25
+fps = 20000
 game = Tetris(height, width)
 counter = 0
 
@@ -165,13 +175,13 @@ while not done:
     if counter > 100000:
         counter = 0
 
-    if counter % (fps ) == 0:
+    if counter % 1 == 0:
         if game.state == "start":
             movement = movementCode[movepos]
             movepos += 1
             movepos = movepos % len(movementCode) 
-            print(movepos)
-            print(movement)
+            #print(movepos)
+            #print(movement)
 
             if movement == "<":
                 game.go_side(-1)
